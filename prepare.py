bf6a2fdd52184023fc7afb79481164216b9fbd38
RoozbehBandpey <r.bandpey67@gmail.com>
2020-04-12 22:27:10 +0200

    fixed loader

--- prepare.py
+++ prepare.py
@@ -1,18 +1,24 @@
-import quandl
-from pprint import pprint\
-
-# mydata = quandl.get("BITFINEX/ETHUSD")
+import quandl as q
 
 
-
-class loader():
+class Loader():
+	QUERY = "BITFINEX/ETHUSD"
 	def __init__(self):
 		"""
-		Loads data from virenty of sources
+		Loads data from variety of sources
 		"""
+		self.QUERY = "BITFINEX/ETHUSD"
 
 	def get_eth_eur_bitfinex(self):
 		"""
-		Loads eherium vs euro data from BITFINEX
+		Loads Etherium vs euro data from BITFINEX
 		"""		
-		return quandl.get_table("BITFINEX/ETHUSD")
+		return q.get(self.QUERY, returns='numpy')
+
+
+
+
+if __name__ == "__main__":
+	l = loader()
+	data = l.get_eth_eur_bitfinex()
+	print(data)
